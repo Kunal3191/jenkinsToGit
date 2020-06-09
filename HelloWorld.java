50d438b447ec376c29e0716e665391c8085596af
Kunal3191 <51157795+Kunal3191@users.noreply.github.com>
2020-06-08 20:31:19 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("-------Hello Jenkins and Git as a part of the trainings-------");
-		System.out.println("-------Hello Jenkins and Git using Webhook-------");
+		System.out.println("-------Hello Jenkins and Git using Webhookssss-------");
 	}
 }
